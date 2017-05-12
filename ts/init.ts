b39ae6263e14fc1139fc8e661ed5e6cf1a2f8276
Alexandr Kolesnik <alexandr.kolesnik.92@gmail.com>
2017-05-12 02:36:03 -0700

    Typescriptified init file

--- ts/init.ts
+++ ts/init.ts
@@ -1,47 +1,48 @@
 import Area from './Area';
 import { enemies } from './enemies';
 
-const selectedAreas = [];
-const Areas = [];
-const fightList = [];
-const mode = 'encounters';
-
 function createAreaSelector(areas: Area[]) {
   const select = document.getElementById('area');
   for (const area in areas) {
-    const option = document.createElement('option');
-    option.innerHTML = area;
-    option.value = area;
-    option.data = areas[area];
-    select.appendChild(option);
+    if (areas.hasOwnProperty(area)) {
+      const option = document.createElement('option');
+      option.innerHTML = area;
+      option.value = area;
+      select.appendChild(option);
+    }
   }
 }
 
 function createEnemyGroupSelector(enemies, areas: Area[]) {
-  const areasSelect = document.getElementById('area');
+  const areasSelect = document.getElementById('area') as HTMLSelectElement;
   const area = areasSelect.options[areasSelect.selectedIndex].value;
-  const select = document.getElementById('enemyGroup');
+  const select = document.getElementById('enemyGroup') as HTMLSelectElement;
   // Clear
   while (select.options.length > 0) {
       select.remove(0);
   }
   for (const enemyGroup in areas[area].encounterTable) {
-    const option = document.createElement('option');
-    option.innerHTML = areas[area].encounterTable[enemyGroup].name;
-    option.value = areas[area].encounterTable[enemyGroup].name;
-    option.data = areas[area].encounterTable[enemyGroup];
-    select.appendChild(option);
+    if (areas[area].encounterTable.hasOwnProperty(enemyGroup)) {
+      const option = document.createElement('option');
+      option.innerHTML = areas[area].encounterTable[enemyGroup].name;
+      option.value = areas[area].encounterTable[enemyGroup].name;
+      select.appendChild(option);
+    }
   }
 }
 
 function initAreas(enemies) {
   const areas = {};
   for (const area in enemies) {
-    areas[area] = new Area(area, enemies[area]);
+    if (enemies.hasOwnProperty(area)) {
+      areas[area] = new Area(area, enemies[area]);
+    }
   }
   return areas;
 }
 
-Areas = initAreas(enemies);
+export const Areas: Area {} = initAreas(enemies);
+export let mode: string = 'encounters';
+
 createAreaSelector(Areas);
 createEnemyGroupSelector(enemies, Areas);
