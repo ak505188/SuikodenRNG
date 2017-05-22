b616468657e5bff2100c8d1bd62a500dba593d67
Alexandr Kolesnik <alexandr.kolesnik.92@gmail.com>
2017-05-22 16:27:35 -0700

    DynamicTable class created

---
+++ ts/DynamicTable.ts
@@ -0,0 +1,28 @@
+import * as $ from 'jquery';
+import Fight from './Fight';
+
+export default class DynamicTable {
+  public id: string;
+  public sel: number = 0;
+
+  constructor(id: string) {
+    this.id = `#${id}`;
+  }
+
+  public generateTable(data: (string | number)[][], headers?: string[]) {
+    const table = $(this.id).empty();
+    for (const r of data) {
+      const row = $('<tr/>');
+      for (const c of r) {
+        row.append($(`<td>${c}</td>`));
+      }
+      table.append(row);
+    }
+  }
+
+  public selectRow(row: number) {
+    $(`${this.id} tr`).eq(this.sel).removeClass('selected');
+    $(`${this.id} tr`).eq(row).addClass('selected');
+    this.sel = row;
+  }
+}
